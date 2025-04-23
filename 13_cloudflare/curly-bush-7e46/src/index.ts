d4ab74921037f15b0fedded47384a221443fe43a
Vishal Rajput <vishalrajput.7vr@gmail.com>
2025-04-23 11:58:24 +0530

    cloudfare workers done

---
+++ 13_cloudflare/curly-bush-7e46/src/index.ts
@@ -0,0 +1,37 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run `npm run dev` in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run `npm run deploy` to publish your worker
+ *
+ * Bind resources to your worker in `wrangler.jsonc`. After adding bindings, a type definition for the
+ * `Env` object can be regenerated with `npm run cf-typegen`.
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+/*🎉  SUCCESS  Application created successfully!
+
+💻 Continue Developing
+Change directories: cd curly-bush-7e46
+Start dev server: npm run start
+Deploy: npm run deploy
+ */
+
+export default {
+	async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+		console.log(request.body);
+		console.log(request.headers);
+
+		if (request.method === "GET") {
+			return Response.json({
+				message: "you sent a get request"
+			});
+		} else {
+			return Response.json({
+				message: "you did not send a get request"
+			});
+		}
+	},
+};
